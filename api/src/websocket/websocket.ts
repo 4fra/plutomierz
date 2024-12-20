384a67efcfd5fce4465374f074bf1625ffe64574
Danrejk <SDanrejk@gmail.com>
2024-12-20 21:05:01 +0100

    Added sending current event title and description

--- api/src/websocket/websocket.ts
+++ api/src/websocket/websocket.ts
@@ -8,6 +8,7 @@ import broadcastActiveUsersCount from "./utils/activeUsersCount";
 import handleChatMessage from './utils/handleChatMessage';
 import handleGetPlutaLog from './utils/handleGetPlutaLog';
 import {savePlutaToDb} from "../db/plutaLogDb";
+import {getCurrentEventTitleAndDescription} from "../db/handleEventsDb";
 
 // pluta value
 updatePlutaValue().then(r => r);
@@ -40,6 +41,10 @@ wss.on('connection', async (ws: WebSocket) => {
     } catch (error) {
         ws.send(JSON.stringify({type: 'error', message: 'Failed to fetch message history'}));
     }
+
+    // send current event title and description
+    const eventTitleAndDescription = await getCurrentEventTitleAndDescription();
+    ws.send(JSON.stringify({type: 'currentEventDescription', title: eventTitleAndDescription.title, description: eventTitleAndDescription.description}));
 
     // handle incoming messages
     ws.on('message', async (data: string) => {
--- web/src/components/splash/Splash.jsx
+++ web/src/components/splash/Splash.jsx
@@ -1,14 +1,24 @@
 import motivationalPlutaJSON from "../../../../android/app/src/main/assets/motivationalPluta.json";
 import {useEffect, useState} from "react";
 import "./Splash.css"
+import {useWebSocketContext} from "../../utils/websocketContext.jsx";
 
 function Splash() {
     const motivationalPluta = motivationalPlutaJSON;
     const [splashText, setSplashText] = useState("");
 
+    const {lastMessage} = useWebSocketContext();
+
     useEffect(() => {
         setSplashText(motivationalPluta[Math.floor(Math.random() * motivationalPluta.length)]);
-    })
+    }, []);
+
+    useEffect(() => {
+        if (lastMessage !== null && JSON.parse(lastMessage.data).type === 'currentEventDescription') {
+            setSplashText("EVENT: "+JSON.parse(lastMessage.data).description);
+            console.log(JSON.parse(lastMessage.data))
+        }
+    }, [lastMessage]);
 
     return (
         <div className={"splash"}>
